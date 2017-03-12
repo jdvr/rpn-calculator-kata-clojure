6abd725eeacd92f29efcde0da6465c83f0042bcd
Juan David Vega <jdvr.93@hotmail.com>
2017-03-12 09:14:21 +0100

    green: given a sum with two digits should return the sum of digits

--- src/rpn_kata_clojure/core.clj
+++ src/rpn_kata_clojure/core.clj
@@ -1,5 +1,20 @@
 (ns rpn-kata-clojure.core
   (:gen-class))
 
+(defn- is-operation? [e]
+  (or (= '+ e) (= '/ e) (= '* e) (= '- e)))
+
+(def operation-fn {
+                   '+ +
+                   '- -
+                   '* *
+                   '/ /
+                   })
+
 (defn rpn [expession]
-  expession)
+  (reduce (fn [acc e]
+            (if (is-operation? e)
+              (conj [] (apply (operation-fn  e) acc))
+              (conj acc e))
+            )
+          [] expession))
--- test/rpn_kata_clojure/core_test.clj
+++ test/rpn_kata_clojure/core_test.clj
@@ -2,8 +2,19 @@
   (:require [clojure.test :refer :all]
             [rpn-kata-clojure.core :refer :all]))
 
-(deftest a-test
+(deftest given-digits
   (testing "given a digit should return the same digit"
     (is (= '(1) (rpn '(1))))
     )
+
+  (testing "given three digit without operation should return the input"
+    (is (= '(1 2) (rpn '(1 2))))
+    )
+  )
+
+(deftest given-simple-operation
+  (testing "given a sum with two digits should return the sum of digits"
+    (is (= '(3) (rpn '(1 2 +))))
+    )
+
   )
